46d4c2cb9b46ce427eb98513511b549fcd8d3047
mesu <mesu.s.ss@gmail.com>
2024-03-20 05:53:39 +0600

    update navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}> 
+      <nav>navbar</nav>
+      {children}
+      <footer>footer</footer> 
+      </body>
     </html>
   );
 }
