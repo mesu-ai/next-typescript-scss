954ab229a92a5503f0f68a842797438ccda19246
Md Ibrahim Adham Mesu <72233105+mesu-ai@users.noreply.github.com>
2024-03-24 08:52:54 +0000

    update navbar

---
+++ src/assets/svgs/AAA.tsx
@@ -0,0 +1,16 @@
+const AAAA = ({ height = 24, width = 24, className = '', color = 'green' }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      className={className}
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg" >
+
+
+    </svg>
+  );
+};
+
+export default AAAA;
